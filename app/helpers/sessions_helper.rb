bb9451425ae3e647fd63ab1b66db259ab6bbd3ca
nakamura <cousaka0623@gmail.com>
2020-10-03 09:27:00 +0000

    migrate edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def current_staff
-    @current_staff ||= Staff.find_by(staff_id: session[:staff_id])
+    @current_staff ||= Staff.find_by(id: session[:staff_id])
   end
 
   def logged_in?
