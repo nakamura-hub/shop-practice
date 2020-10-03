80d62a15459a2f2c13bd987ca93c176c5f0d5fbf
nakamura <cousaka0623@gmail.com>
2020-10-03 09:44:33 +0000

    migrate edit

--- db/migrate/20200921162832_change_data_born_to_customer.rb
+++ db/migrate/20200921162832_change_data_born_to_customer.rb
@@ -1,5 +1,5 @@
 class ChangeDataBornToCustomer < ActiveRecord::Migration[5.2]
   def change
-    change_column :customers, :born, :date, using: "born::date"
+    change_column :customers, :born, :date, 'date USING CAST(born AS date)'
   end
 end
